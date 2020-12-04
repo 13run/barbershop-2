a0f9c8e57b9ca62fcfa9ca926715eb427d6d1619
13RuN <runemal13@gmail.com>
2020-12-04 15:12:58 +0500

    add client entity & update gemfile

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
 
+set :database, 'sqlite3:barbershop.db'
+
+class Client < ActiveRecord::Base
+
+end
 
 
 get '/' do
